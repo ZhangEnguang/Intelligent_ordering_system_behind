c8ee3788e3de8160e5cac2eb470878cc5c2cc6a9
zhangeg <1qaz!QAZ>
2020-04-01 01:55:48 +0800

    小改

--- src/main/java/com/tsc/iorder/controller/CarouselController.java
+++ src/main/java/com/tsc/iorder/controller/CarouselController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.Console;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
